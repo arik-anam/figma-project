4fbb84a2ce5c386ef6061bbc8d84971f153faa24
arik.project <146571996+arik17940@users.noreply.github.com>
2024-09-21 15:25:22 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "contact(1)": "url('./img/ba')",
+        "footer-texture": "url('/img/footer-texture.png')",
+      },
+    },
+  },
+  plugins: [],
+};
